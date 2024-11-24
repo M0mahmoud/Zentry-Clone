61b82fd09b7a4e95426471d8c4b4f3f0a8a7597f
M0mahmoud <houdmohamed85@gmail.com>
2024-11-24 23:58:13 +0200

    About Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 function App() {
     return (
         <main className="relative w-screen min-h-screen overflow-x-hidden">
             <Hero />
+            <About />
             <section className=" min-h-[200vh]" />
         </main>
     );
